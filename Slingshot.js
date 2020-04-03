dcd2c19f42680b6880e1f6d85f80fb543016cb7c
Paehel Shah <shahpaehel@gmail.com>
2020-04-03 15:41:33 +0530

    stage2

--- Slingshot.js
+++ Slingshot.js
@@ -25,41 +25,31 @@ class SlingShot{
 
   
     display(){
-        image(this.sling1,200,20);
-        image(this.sling2,170,20);
+        image(this.sling1,150,420);
+        image(this.sling2,120,420);
 
   
     	if(this.sling.bodyA){
     		var pointA = this.sling.bodyA.position;
-    	    var pointB = this.pointB;
-    		strokeWeight(3);
-    		stroke("blue");
-    		line(pointA.x, pointA.y, pointB.x, pointB.y);
-    	}
-        else{
-            textSize(18);
-            fill(0);
-            text("Press space to reattach the ball", 200, 50);
-        }
-       
-        push();
-            
-        stroke(48,22,8);
-        if(pointA.x < 220) {
+            var pointB = this.pointB;
+              push();
+             stroke(48,22,8);
+        if(pointA.x < 180) {
             strokeWeight(7);
-            line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
-            line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
+            line(pointA.x - 20, pointA.y , pointB.x + 60, pointB.y - 30);
+            line(pointA.x -20 , pointA.y, pointB.x + 10 , pointB.y -20 );
             image(this.sling3,pointA.x -30, pointA.y -10,15,30);
         }
         else{
-            strokeWeight(3);
-            line(pointA.x + 25, pointA.y, pointB.x -10, pointB.y);
-            line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y - 3);
+            strokeWeight(7);
+            line(pointA.x + 25, pointA.y, pointB.x +60, pointB.y-30);
+            line(pointA.x + 25, pointA.y, pointB.x + 10, pointB.y - 20);
             image(this.sling3,pointA.x + 25, pointA.y -10,15,30);
+           
         }
        
         
         pop();
     }
-    
+    } 
 }
